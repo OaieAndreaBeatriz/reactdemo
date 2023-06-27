210a96a86ce6993b01451c16be85d499d78944a8
Andrea Oaie Beatriz <oaie.andrea.beatriz@gmail.com>
2023-06-27 15:06:51 +0300

    All files added

---
+++ src/Themes.js
@@ -0,0 +1,9 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+    typography: {
+        fontFamily: 'Caprasimo, sans-serif' },
+        fontSize: '10rem',
+});
+
+export default theme
